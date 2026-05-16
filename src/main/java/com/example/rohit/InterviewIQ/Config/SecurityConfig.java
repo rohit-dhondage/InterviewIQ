ad3c07fbcec071cdead81771adff35c2a61c1b99
Rohit Dhondage <rohitdhondage110@gmail.com>
2026-05-16 19:26:42 +0530

    Makes Http Request Stateless - session

--- src/main/java/com/example/rohit/InterviewIQ/Config/SecurityConfig.java
+++ src/main/java/com/example/rohit/InterviewIQ/Config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,7 +29,12 @@ JwtFilter jwtFilter;
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth**","/auth/register", "/auth/login").permitAll()
                         .anyRequest().authenticated()
+
+                        // stateless Jwt requst 
+                ).sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
+                //Run JwtFilter before Spring login filter
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
 
